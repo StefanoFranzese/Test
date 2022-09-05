c10791f63f48f8520a3e7956f19f19145f870b8c
franzese9 <franzese9@libero.it>
2022-09-05 10:45:35 +0200

    parte1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div class=" container one">
+        <div class="row" >
+          <div class=" text1container">
+            <h2 class="text1">
+              EXPERT WORKSHOP: AI <br/>
+              BASED WORKER<br/> 
+              MANAGEMENT AND OSH
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div class=" container-fluid">
+        <div class="row timer">
+          <div class="col-3"></div>
+          <div class="col-6">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">15</th>
+                  <th scope="col">12</th>
+                  <th scope="col">45</th>
+                  <th scope="col">16</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Day</td>
+                  <td>Hours</td>
+                  <td>Minutes</td>
+                  <td>Second</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-3"></div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, SocialBar } from "./links";
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const linkContainerRef = useRef(null);
+  const linkListRef = useRef(null);
+
+  useEffect(() => {
+    const linkListHeight = linkListRef.current.getBoundingClientRect().height;
+    if (show) {
+      linkContainerRef.current.style.height = `${linkListHeight}px`;
+    } else {
+      linkContainerRef.current.style.height = "0px";
+    }
+  }, [show]);
+  return (
+    <nav className="nav">
+      <header className="nav-header">
+        <div className="nav-brand"></div>
+        <button className="btn nav-toggler" onClick={() => setShow(!show)}>
+          <FaBars className="nav-icon" />
+        </button>
+      </header>
+      <div
+        className={`${show ? "links-container show" : "links-container"}`}
+        ref={linkContainerRef}
+      >
+        <ul className="nav-links" ref={linkListRef}>
+          {links.map((el) => (
+            <li key={el.id}>
+              <a href={el.url} alt={el.text}>
+                {el.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="social-links">
+        <SocialBar />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
